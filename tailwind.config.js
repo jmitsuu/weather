482d47bacfba3649e96ffd83207d2bad2771decc
Jairo Miguel <78458400+jmitsuu@users.noreply.github.com>
2023-07-25 00:28:04 -0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,9 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'bgSun': "url('../src/assets/images/sun.png')",
-        'bgRain': "url('../src/assets/images/rain.png')",
-        'bgMist': "url('../src/assets/images/mist.png')",
+        'bgSun': "url('/src/assets/images/sun.png')",
+        'bgRain': "url('/src/assets/images/rain.png')",
+        'bgMist': "url('/src/assets/images/mist.png')",
         'footer-texture': "url('/img/footer-texture.png')",
       }
     },
